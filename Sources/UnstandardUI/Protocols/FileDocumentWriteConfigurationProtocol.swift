08cff502edc19df8cef72e97372b6476c4549d6a
Christopher Weems <hello@christopherweems.com>
2021-09-15 13:32:05 -0400

    Add `FileDocumentReadConfigurationProtocol` & `FileDocumentWriteConfigurationProtocol`

---
+++ Sources/UnstandardUI/Protocols/FileDocumentWriteConfigurationProtocol.swift
@@ -0,0 +1,23 @@
+//
+//  FileDocumentWriteConfigurationProtocol.swift
+//
+//
+//  Created by Christopher Weems on 9/10/20.
+//
+
+import struct SwiftUI.FileDocumentWriteConfiguration
+import UniformTypeIdentifiers
+
+@available(macOS 11, iOS 14, tvOS 14, watchOS 7, *)
+public protocol FileDocumentWriteConfigurationProtocol {
+    /// The uniform type of the file contents.
+    var contentType: UTType { get }
+    
+    /// The file wrapper containing the current document content.
+    /// `nil` if the document is unsaved.
+    var existingFile: FileWrapper? { get }
+    
+}
+
+@available(macOS 11, iOS 14, tvOS 14, watchOS 7, *)
+extension FileDocumentWriteConfiguration: FileDocumentWriteConfigurationProtocol { }
