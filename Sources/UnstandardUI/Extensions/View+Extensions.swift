9d2c81085110a2350463720e7dc54c91c9dcb671
Christopher Weems <hello@christopherweems.com>
2021-09-16 14:39:08 -0400

    Add `View.task(geometryID:priority:_:)`

--- Sources/UnstandardUI/Extensions/View+Extensions.swift
+++ Sources/UnstandardUI/Extensions/View+Extensions.swift
@@ -7,6 +7,29 @@
 
 import SwiftUI
 
+// MARK: - Asynchronous updates triggered by changes of geometry
+
+extension View {
+    @available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+    public func task<V>(geometryID valuePath: KeyPath<GeometryProxy, V>,
+                        priority: TaskPriority = .userInitiated,
+                        _ action: @escaping (V) async -> Void) -> some View where V : Equatable {
+        self.background(
+            GeometryReader { geometry in
+                let geometryValue = geometry[keyPath: valuePath]
+                
+                Color.clear
+                    .task(id: geometryValue, priority: priority, { await action(geometryValue) })
+                
+            }
+        )
+    }
+    
+}
+
+
+// MARK: - Type Erasure
+
 extension View {
     public func eraseToAnyView() -> AnyView {
         .init(self)
