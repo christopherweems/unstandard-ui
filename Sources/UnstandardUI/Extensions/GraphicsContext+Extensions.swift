5ec941cde1bc03a33b48dfffd1b5ef1c609c266e
Christopher Weems <hello@christopherweems.com>
2021-10-22 08:18:56 -0400

    Add `GraphicsContext.draw(_image:within:)`

---
+++ Sources/UnstandardUI/Extensions/GraphicsContext+Extensions.swift
@@ -0,0 +1,32 @@
+//
+// GraphicsContext+Extensions.swift
+//  
+//
+//  Created by Christopher Weems on 10/21/21.
+//
+
+import SwiftUI
+
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+extension GraphicsContext {
+    // needs more testing & non-resolved image variations before being complete
+    public func draw(_image image: GraphicsContext.ResolvedImage, within frame: CGRect) {
+        let imageAspectRatio = image.size.aspectRatio
+        let frameAspectRatio = frame.size.aspectRatio
+        
+        let drawingFrame: CGRect
+        
+        if imageAspectRatio < frameAspectRatio {
+            // image taller than frame, drawing frame should narrow
+            drawingFrame = frame.insetBy(dx: frame.height * (frameAspectRatio - imageAspectRatio) / 2, dy: 0)
+            
+        } else {
+            // image wider than frame, drawing frame should shorten
+            drawingFrame = frame.insetBy(dx: 0, dy: frame.height * (imageAspectRatio - frameAspectRatio) / 4)
+            
+        }
+        
+        self.draw(image, in: drawingFrame, style: .init(antialiased: true))
+    }
+    
+}
