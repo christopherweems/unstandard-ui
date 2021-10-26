d68fe99c694185986b9f3242f42967948c073775
Christopher Weems <hello@christopherweems.com>
2021-10-26 16:37:29 -0400

    Add `EdgeInsets` `Hashable` conformance

--- Sources/unstandardUI/Extensions/EdgeInsets+Extensions.swift
+++ Sources/unstandardUI/Extensions/EdgeInsets+Extensions.swift
@@ -11,3 +11,14 @@ extension EdgeInsets {
     public static let zero = EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
     
 }
+
+extension EdgeInsets : Hashable {
+    public func hash(into hasher: inout Hasher) {
+        trailing.hash(into: &hasher)
+        top.hash(into: &hasher)
+        leading.hash(into: &hasher)
+        bottom.hash(into: &hasher)
+        
+    }
+    
+}
