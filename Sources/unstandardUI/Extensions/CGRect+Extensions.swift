64046957375700e924c690e704af9b4261067e14
Christopher Weems <hello@christopherweems.com>
2021-10-26 17:00:17 -0400

    Add `CGRect.unitRect`

--- Sources/unstandardUI/Extensions/CGRect+Extensions.swift
+++ Sources/unstandardUI/Extensions/CGRect+Extensions.swift
@@ -9,6 +9,10 @@ import SwiftUI
 import unstandard
 
 extension CGRect {
+    public static var unitRect: Self {
+        .init(origin: .zero, size: .init(width: 1, height: 1))
+    }
+    
     public func position(of unitPoint: UnitPoint) -> CGPoint {
         CGPoint(unitPoint * size) + origin
     }
