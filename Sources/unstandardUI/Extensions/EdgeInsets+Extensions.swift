d499e74eee8d67a849e659c948f47ea52766f221
Christopher Weems <hello@christopherweems.com>
2021-10-26 17:28:01 -0400

    Add `EdgeInsets.offset(at:)`, `.mirrored(across:)` & `*` operator

--- Sources/unstandardUI/Extensions/EdgeInsets+Extensions.swift
+++ Sources/unstandardUI/Extensions/EdgeInsets+Extensions.swift
@@ -12,6 +12,65 @@ extension EdgeInsets {
     
 }
 
+
+// MARK: - Operators
+
+extension EdgeInsets {
+    public static func *(_ lhs: Self, _ rhs: Self) -> Self {
+        EdgeInsets(top: lhs.top * rhs.top, leading: lhs.leading * rhs.leading, bottom: lhs.bottom * rhs.bottom, trailing: lhs.trailing * rhs.trailing)
+    }
+    
+}
+
+
+// MARK: - Offset
+
+extension EdgeInsets {
+    public func offset(at corner: UnitPoint) -> UnitPoint {
+        switch corner {
+        case .trailing:
+            return .init(x: -trailing, y: 0)
+            
+        case .topTrailing:
+            return .init(x: -trailing, y: top)
+            
+        case .top:
+            return .init(x: 0, y: top)
+            
+        case .topLeading:
+            return .init(x: leading, y: top)
+            
+        case .leading:
+            return .init(x: leading, y: 0)
+            
+        case .bottomLeading:
+            return .init(x: leading, y: -bottom)
+            
+        case .bottom:
+            return .init(x: 0, y: -bottom)
+            
+        case .bottomTrailing:
+            return .init(x: -trailing, y: -bottom)
+            
+        default:
+            fatalError()
+        }
+    }
+    
+}
+
+extension EdgeInsets {
+    @available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+    public func mirrored(across _: HorizontalEdge) -> EdgeInsets {
+        return .init(top: top, leading: trailing, bottom: bottom, trailing: leading)
+    }
+    
+}
+
+
+// MARK: - Hashable conformance
+
+
 extension EdgeInsets : Hashable {
     public func hash(into hasher: inout Hasher) {
         trailing.hash(into: &hasher)
