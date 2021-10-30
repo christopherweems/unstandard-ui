6b9254e1a4f45c44e9b9525ac28b228c5257ab82
Christopher Weems <hello@christopherweems.com>
2021-10-30 00:55:57 -0400

    Add `View.when(_:perform:)`

---
+++ Sources/unstandardUI/Extensions/View+WhenOccurs.swift
@@ -0,0 +1,40 @@
+//
+//  View+When.swift
+//  
+//
+//  Created by Christopher Weems on 10/29/21.
+//
+
+import SwiftUI
+
+@available(macOS 11, iOS 14, tvOS 14, watchOS 7, *)
+extension View {
+    // Like `View.onChange(of:perform:)` but guarenteed to occur only the first time predicate becomes true
+    public func when(_ predicate: Bool, perform action: @escaping () -> Void) -> some View {
+        self.modifier(WhenViewModifier(predicate: predicate, action: action))
+    }
+    
+}
+
+@available(macOS 11, iOS 14, tvOS 14, watchOS 7, *)
+internal struct WhenViewModifier: ViewModifier {
+    @State var hasOccurred = false
+    
+    let predicate: Bool
+    let action: () -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                guard predicate else { return }
+                assert(!hasOccurred)
+                action()
+                hasOccurred = true
+            }
+            .onChange(of: predicate == true && !hasOccurred) { _ in
+                guard !hasOccurred else { return }
+                action()
+            }
+    }
+    
+}
