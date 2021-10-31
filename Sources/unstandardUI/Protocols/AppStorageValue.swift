205e6d8444f932fbbb790d74e0cb8afe9e56c2ce
Christopher Weems <hello@christopherweems.com>
2021-10-31 00:47:15 -0400

    Add `AppStoreValue.Options` type

--- Sources/unstandardUI/Protocols/AppStorageValue.swift
+++ Sources/unstandardUI/Protocols/AppStorageValue.swift
@@ -18,6 +18,36 @@ extension AppStorageValue {
     
 }
 
+extension AppStorageValue where Self : Equatable {
+    public typealias Options = AppStorageValueOptions<Self>
+    
+}
+
+public struct AppStorageValueOptions<Value> where Value : AppStorageValue, Value : Equatable {
+    private var allCases: Value.AllCases { Value.allCases }
+    private var index = Value.allCases.startIndex
+    
+    public mutating func next() -> Value? {
+        var nextIndex = allCases.index(after: index)
+        if nextIndex == allCases.endIndex { nextIndex = allCases.startIndex }
+        index = nextIndex
+        return allCases[index]
+    }
+    
+    public init() { }
+    
+    public init(startingAt item: Value?) {
+        if let item = item {
+            index = Value.allCases.firstIndex(of: item)!
+            
+        }
+        
+    }
+}
+
+
+// MARK: -
+
 @available(macOS 11, iOS 14, tvOS 14, *)
 extension AppStorage {
     public init(wrappedValue: Value, store: UserDefaults? = nil) where Value : AppStorageValue, Value.RawValue == String {
