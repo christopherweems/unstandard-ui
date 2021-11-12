52391eb9abf53975ecb509ad8539315a6dcbd793
Christopher Weems <hello@christopherweems.com>
2021-11-12 13:36:45 -0500

    Add `CGSize.infinite`

---
+++ Sources/unstandardUI/Extensions/CGSize+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  CGSize+Extensions.swift
+//  
+//
+//  Created by Christopher Weems on 11/12/21.
+//
+
+import Foundation
+
+extension CGSize {
+    public static var infinite: CGSize {
+        .init(width: CGFloat.infinity, height: CGFloat.infinity)
+    }
+    
+}
