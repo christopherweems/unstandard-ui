61bfdd2684a6987b76b94d3a0b810821b84bc020
Christopher Weems <hello@christopherweems.com>
2021-11-12 13:43:54 -0500

    Add `CanvasProxy` type

---
+++ Sources/unstandardUI/Data Types/CanvasProxy.swift
@@ -0,0 +1,14 @@
+//
+//  CanvasProxy.swift
+//  
+//
+//  Created by Christopher Weems on 11/12/21.
+//
+
+import SwiftUI
+
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public struct CanvasProxy {
+    public let size: CGSize
+    
+}
