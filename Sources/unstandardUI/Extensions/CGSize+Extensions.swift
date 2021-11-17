d04520d1794ba7c4f883b31819585f34b5545729
Christopher Weems <hello@christopherweems.com>
2021-11-17 10:54:06 -0500

    Add `CGSize.length(along: Axis)`

--- Sources/unstandardUI/Extensions/CGSize+Extensions.swift
+++ Sources/unstandardUI/Extensions/CGSize+Extensions.swift
@@ -7,9 +7,27 @@
 
 import SwiftUI
 
+
+// MARK: -
+
 extension CGSize {
     public static var infinite: CGSize {
         .init(width: CGFloat.infinity, height: CGFloat.infinity)
     }
     
+}
+
+
+// MARK: -
+
+extension CGSize {
+    public func length(along axis: Axis) -> CGFloat {
+        switch axis {
+        case .horizontal:
+            return width
+            
+        case .vertical:
+            return height
+        }
+    }
 }
